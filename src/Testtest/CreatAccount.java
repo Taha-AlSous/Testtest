4b40e774d18daac39530d6c2ed49b5734ba2806a
Mohammad-KhairBaalbaki <mohammad.k.baalbaki@gmail.com>
2024-12-23 11:39:06 +0300

    edit the exist user

--- src/Testtest/CreatAccount.java
+++ src/Testtest/CreatAccount.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
@@ -207,6 +208,19 @@ public class CreatAccount extends JFrame implements ActionListener ,MouseListene
         {
             if(Second.checkUsernameAndPassword(username, password))
             {
+                if(!App.users.isEmpty())
+                {
+                    for(User user :App.users)
+                    {
+                        if(user.getUsername().equals(username) && user.getPassword().equals(password))
+                        {
+                            JOptionPane.showMessageDialog(null,"This User is already exist !\nPlease Login","User exist",JOptionPane.WARNING_MESSAGE);
+                            this.setVisible(false);
+                            return true;
+                        }
+                    }
+                
+                }
                 App.users.add(new User(username, password));
                 App.saveUsernamesAndPasswords();
                 this.setVisible(false);
